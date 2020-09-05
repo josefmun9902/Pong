37f56935804bc1e3250dba2ac92f5ba572c7d5e6
Josef <josefmun9902@gmail.com>
2020-09-05 10:13:42 -0500

    Update Ball.java

--- src/Ball.java
+++ src/Ball.java
@@ -1,30 +1,3 @@
-//done, at least I think so....
-/*Master's Notes
-//
-//
--90 degrees is down, 270 is up, 0 is right, 180 is left
-//
-//
-//
-WALLS
--figure out where the ball hits the wall(not where the ball is, where the ball should have hit)
--use hit y, (the wall height)
--delta y=sy-ey
--delta s=sx-ex
--delta y to hit=sy-hy
--%2h = delta y 2 hit/ delta y
--delta x to hit = delta a *%2h
--hx=sx+delta x hit
--usedPeed=sqrt of (delta x to hit)squared+(delta y to hit)squared
--leftOverSpeed=speed-usedSpeed
--then the bottom wall is the ball height and y of bottom wall
-PADDLES
--hit x=px-bw
--for the other paddle x is y and y is x
-//
-//
-//
-*/
 import java.awt.*;
 public class Ball
 {
